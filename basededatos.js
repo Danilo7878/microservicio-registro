23e9d4be73277362c053669b1e22524a108ed693
Danilo7878 <danilosandovalle@gmail.com>
2021-07-12 19:25:27 -0600

    db string connection

--- basededatos.js
+++ basededatos.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://proyecto:proyecto123@docdb-2021-07-12-17-51-48.capzci6favog.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&retryWrites=false', {
+mongoose.connect('mongodb+srv://alex:alexander123@cluster0.f3k1w.mongodb.net/Petroll?retryWrites=true&w=majority', {
     useNewUrlParser: true
 }) 
     .then(db => console.log(`Base de datos conectada conectada en Mongo DB Atlas`))
